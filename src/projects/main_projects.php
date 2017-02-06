89d0fbd7bdb961d85dc6940e19d291ebb5e90e64
Joel Dorne <joel@objective.agency>
2017-02-06 16:45:07 +0800

    Issue for Ray

---
+++ src/projects/main_projects.php
@@ -0,0 +1,11 @@
+<?php require_once('head/main_head.php'); ?>
+
+<?php require_once('menu/main_menu.php'); ?>
+
+<?php require_once('projects/items/archive_project_modal.php'); ?>
+
+<?php require_once('projects/items/project_header.php'); ?>
+
+<?php require_once('projects/items/project_folders.php'); ?>
+
+<?php require_once('footer/main_footer.php');?>
